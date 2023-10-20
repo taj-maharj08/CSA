42333facba1fc324d8484c0bc3908b1a647a16fd
taj-maharj08 <122415382+taj-maharj08@users.noreply.github.com>
2023-10-20 13:56:23 -0400

    Final Cylinder class

--- Cylinder.java
+++ Cylinder.java
@@ -1,26 +1,18 @@
 public class Cylinder{
-    private double base;
+    private Circle base;
     private double height;
 
     public Cylinder(double rad, double height){
-        Circle cir = new Circle(rad);
-        base = cir.area();
+        base = new Circle(rad);
         this.height = height;
     }
 
     public double vol(){
-        double v = base*height;
+        double v = base.area()*height;
         return v;
     }
 
     public double sa(){
-        return (cir.circumference()*height)+(base*2);
+        return (base.circumference()*height)+(base.area()*2);
     }
 }
-
-
-/*·       Cylinder should have 2 attributes only, a base of type Circle and a height (double)
-·       Constructor should take 2 args, radius and height(in that order) and initialize the attributes.  Don’t forget to use new to create the circle.
-·       Methods that return the volume (vol) and surface area (sa) (both double).  These methods should not take arguments, but should use the class attributes to compute.
-·       Use the CylinderTester class provided to test your classes.  Check your work with a calculator.
- */
